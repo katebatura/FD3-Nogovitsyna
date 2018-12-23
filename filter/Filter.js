b2b16472e205b8beba9608af81418e25930b59ac
Kate <katebatura@gamil.com>
2018-12-24 01:16:14 +0300

    filter

--- filter/Filter.js
+++ filter/Filter.js
@@ -4,31 +4,52 @@ var Filter = React.createClass({
 
     propTypes: {
         words: React.PropTypes.arrayOf(
-            React.PropTypes.shape({
-                key: React.PropTypes.number.isRequired,
-                name: React.PropTypes.string.isRequired,
-            })
+            React.PropTypes.string.isRequired,
         )
     },
 
     getInitialState: function() {
         return { 
             wordsArr: this.props.words,
-            selectedCheckbox: null,
-            freeText: null 
+            selectedCheckbox: false,
+            freeText: '' 
         };
       },
 
+    sortWords: function() {
+        this.state.selectedCheckbox ? 
+        this.setState( {selectedCheckbox: false}, this.processWords ) :
+        this.setState( {selectedCheckbox: true}, this.processWords );
+    },
+
+    filterWords: function(e) {
+        this.setState( {freeText: e.target.value}, this.processWords );
+    },
+
+    processWords: function() {
+        let wordsArr = this.props.words.slice();
+
+        if(this.state.selectedCheckbox) {
+            wordsArr.sort();
+        }
+
+        if(this.state.freeText) {
+            wordsArr = wordsArr.filter( v => v.indexOf(this.state.freeText) != -1 );
+        }
+
+        this.setState( {wordsArr: wordsArr} );
+    },
+
     render: function() {
 
-        var optionsGroup = this.props.words.map( v => 
-            React.DOM.option( {key: v.key}, v.name),
+        var optionsGroup = this.state.wordsArr.map( ( v, i ) => 
+            React.DOM.option( {key: i}, v),
         );
 
         return React.DOM.div( null, 
             React.DOM.div(null, 
                 React.DOM.input( {type: 'checkbox', onClick: this.sortWords} ),
-                React.DOM.input( {type: 'text', onChange: this.sortWords} )
+                React.DOM.input( {type: 'text', onChange: this.filterWords} )
             ),            
             React.DOM.select( {multiple: true, size: '10'}, optionsGroup )
             )        
